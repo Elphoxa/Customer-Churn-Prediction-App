4443117142966118a081eb4c4bfe2cb38e28daf1
Efosa Dave Omosigho <efosaomosigho@gmail.com>
2024-02-18 21:34:36 +0100

    Add pages sub-directory and main.py

---
+++ pages/01_Data.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(
+    page_title='View Data',
+    page_icon=':)',
+    layout='wide'
+)
+
+st.title('Exploratory Data Analysis: Insights from Vodafone Data')
---
+++ pages/02_Dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.set_page_config(
+    page_title='Dashboard',
+    page_icon=':)',
+    layout='wide'
+)
+
+st.title('Vodafone Data Insights Dashboard')
